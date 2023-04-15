a0e9a2cbb734acc266ed72ba90dc503ec739c32b
KONE Souleymane <souleymane.kone@smile.ci>
2023-04-15 15:41:03 +0000

    start deploy precess

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import {SectionWrapper, Download, Features} from "./components";
+import assets from "./assets";
+import styles from "./styles/Global";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    return (
+        <>
+            <SectionWrapper
+                title="Your own store of Nifty NFT store. Start Selling & Growing. "
+                description="Buy, store, collect NFTS, exchange & earn crypto. Join 25+ million people using ProNerf Marketplace."
+                showBtn
+                mockupImg={assets.homeHero}
+                banner="banner"
+            />
+            <SectionWrapper
+                title="Smart User Interface Marketplace"
+                description="Experience a buttery UI of ProNef NTF Marketplace. Smooth constant colors of a fluent UI design."
+                mockupImg={assets.homeCards}
+                reverse
+            />
+            <Features/>
+            <SectionWrapper
+                title="Deployement"
+                description="ProNerf is build using Expo which runs natively on all users' devices. You can easily get your app into people hands"
+                showBtn
+                mockupImg={assets.feature}
+                reverse
+            />
+            <SectionWrapper
+                title="Creative way to showcase the store"
+                description="The app contains two screens. The first lists all NFTs while the second one shows the details of a specific NFT."
+                mockupImg={assets.mockup}
+                banner="banner02"
+            />
+            <Download  />
+            <div className="px-4 py-2 justify-center items-center bg-primary flex-col text-center banner04">
+                <p className={`${styles.pText} ${ styles.whiteText }`}>Made with love by { " "}
+                      <span className="bold">ApiMan</span>
+                </p>
+            </div>
+        </>
+    );
 }
 
 export default App;
---
+++ src/components/SectionWrapper.jsx
@@ -0,0 +1,32 @@
+import React, {Component} from 'react';
+import styles from "../styles/Global";
+import assets from "../assets";
+import {Button} from "./index";
+
+const SectionWrapper = ({title, description, showBtn, mockupImg, banner, reverse}) => {
+    return (
+        <div
+            className={`min-h-screen ${styles.section} ${styles.bgWhite} ${banner} ${reverse ? styles.bgWhite : styles.bgPrimary}`}>
+            <div
+                className={`flex items-center ${reverse ? styles.boxReverseClass : styles.boxClass} w-11/12 sm:w-full md:minmd:w-3/4`}>
+                <div
+                    className={`${styles.descDiv} ${reverse ? "fadeRightMini" : "fadeLeftMini"} ${reverse ? styles.textRight : styles.textLeft}`}>
+                    <h1 className={`
+                    ${reverse ? styles.blackText : styles.whiteText}
+                    ${styles.h1Text}`}>{title}</h1>
+                    <p className={`
+                    ${reverse ? styles.blackText : styles.whiteText}
+                    ${styles.descriptionText}`}>{description}</p>
+                    {showBtn && (
+                        <Button assetUrl={assets.expo} link="Deployed nft macketplace"/>
+                    )}
+                </div>
+                <div className={` flex-1 ${styles.flexCenter} p-8 sm:px-9 ${reverse ? "fadeLeftMini" : "fadeRightMini"}`}>
+                    <img src={mockupImg} alt="mocUp" className={`${styles.sectionImg} `}/>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default SectionWrapper;
